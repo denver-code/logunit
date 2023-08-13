e3f3444c36a350ddf876aae60a1cc766ffa3ae92
Ihor Savenko <csigorek@gmail.com>
2023-08-13 23:36:09 +0100

    new rsa keys generator

--- genrsa.py
+++ genrsa.py
@@ -1,29 +1,58 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.serialization import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives import serialization
 
-publicKey, privateKey = rsa.newkeys(
-  public_exponent=65537,
+
+# publicKey, privateKey = rsa.newkeys(
+#   public_exponent=65537,
+#     key_size=4096,
+#     backend=default_backend()
+# )
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
     key_size=4096,
     backend=default_backend()
 )
+public_key = private_key.public_key()
 
-def write_keys_to_file(publicKey, privateKey):
+def write_keys_to_file(public_key, private_key):
+    private_pem = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.PKCS8,
+        encryption_algorithm=serialization.NoEncryption()
+    )
+    public_pem = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo
+    )
     with open('public.pem', 'wb') as pub_file:
-        pub_file.write(publicKey.save_pkcs1())
+        pub_file.write(public_pem)
 
     with open('private.pem', 'wb') as priv_file:
-        priv_file.write(privateKey.save_pkcs1())
+        priv_file.write(private_pem)
 
 
 message = "Hello World!"
  
-encMessage = rsa.encrypt(message.encode(),
-                         publicKey)
-decMessage = rsa.decrypt(encMessage, privateKey).decode()
+encMessage = public_key.encrypt(
+    message.encode(),
+    padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )
+)
+decMessage = private_key.decrypt(encMessage, padding.OAEP(
+        mgf=padding.MGF1(algorithm=hashes.SHA256()),
+        algorithm=hashes.SHA256(),
+        label=None
+    )).decode()
 
 assert message == decMessage
 
-write_keys_to_file(publicKey, privateKey)
+write_keys_to_file(public_key, private_key)
 
 print("Keys generated!")
